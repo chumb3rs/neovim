09c7af4c304d49cb7c649a6c66584b163a7d17fa
Nick Marks <nickmarks00@gmail.com>
2023-08-10 14:14:59 +1000

    Re-add telescope, plenary after merge deletion

--- lua/chumbers/packer.lua
+++ lua/chumbers/packer.lua
@@ -7,7 +7,7 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
-  use 'github/copilot.vim'
+    use 'github/copilot.vim'
 
     use {
         'jghauser/fold-cycle.nvim',
@@ -16,7 +16,14 @@ return require('packer').startup(function(use)
         end
     }
 
-     use "EdenEast/nightfox.nvim" -- Packer
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+
+    use "EdenEast/nightfox.nvim" -- Packer
     vim.cmd("colorscheme nordfox")
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
@@ -54,7 +61,7 @@ return require('packer').startup(function(use)
 
     use 'tpope/vim-dispatch'
 
-   use 'SirVer/ultisnips'
+    use 'SirVer/ultisnips'
 
     use({
         "L3MON4D3/LuaSnip",
