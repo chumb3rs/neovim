034cba91ba621cf4f65c53ab38a0f49b2d0b871b
Nick Marks <nickmarks00@gmail.com>
2023-09-20 21:13:43 +1000

    Add better line number coloring

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,8 +2,16 @@ function ColorConfig(color)
 	color = color or "tokyonight"
 	vim.cmd.colorscheme(color)
 
-	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" });
-	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" });
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" });
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" });
+end
+
+-- Sets colors to line numbers Above, Current and Below  in this order
+function LineNumberColors()
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=true })
 end
 
 ColorConfig()
+LineNumberColors()
