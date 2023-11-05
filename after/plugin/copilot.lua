01c11c36be07a87c015168c5b238ac759ee448b9
Nick Marks <nickmarks00@gmail.com>
2023-11-05 18:52:17 +1100

    Update copilot to work in java files

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -2,7 +2,7 @@ vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 vim.g.copilot_filetypes = {
-    ["*"] = false,
+    -- ["*"] = false,
     ["javascript"] = true,
     ["typescript"] = true,
     ["lua"] = false,
