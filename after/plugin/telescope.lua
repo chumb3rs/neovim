0d4bfb1a942778188eda0b5f2c14a5ca953c7acf
Nick Marks <nickmarks00@gmail.com>
2023-11-09 21:03:36 +1100

    Add bulk telescope and DAP remaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -10,11 +10,11 @@ end, { desc = "[P]roject [S]earch" })
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(themes.get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	require('telescope.builtin').current_buffer_fuzzy_find(themes.get_dropdown {
+		winblend = 10,
+		previewer = false,
+	})
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
@@ -23,3 +23,11 @@ vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch curren
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch [B]uffers" })
+vim.keymap.set("n", "<leader>sm", builtin.marks, { desc = "[S]earch [M]arks" })
+vim.keymap.set("n", "<leader>sr", builtin.lsp_references, { desc = "[S]earch LSP [R]eferences" })
+vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols, { desc = "[S]earch LSP Document [S]ymbols" })
+vim.keymap.set("n", "<leader>sc", builtin.lsp_incoming_calls, { desc = "[S]earch LSP Incoming [C]alls" })
+vim.keymap.set("n", "<leader>so", builtin.lsp_outgoing_calls, { desc = "[S]earch LSP [O]utgoing Calls" })
+vim.keymap.set("n", "<leader>si", builtin.lsp_implementations, { desc = "[S]earch LSP [I]mplementations" })
+vim.keymap.set("n", "<leader>sx", "<cmd>Telescope diagnostics bufnr=0<cr>", { desc = "[S]earch LSP errors" })
--- lua/chumbers/remap.lua
+++ lua/chumbers/remap.lua
@@ -78,3 +78,25 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Toggle breakpoint" })
 vim.keymap.set("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start or continue debugger" })
 vim.keymap.set("n", "<leader>dpr", ":lua require('dap-python').test_method() <CR>", { desc = "Run python test method" })
+
+
+vim.keymap.set("n", "<leader>bc", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>",
+    { desc = "Set conditional breakpoint" })
+vim.keymap.set("n", "<leader>bl",
+    "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>", { desc = "Set log point" })
+vim.keymap.set("n", '<leader>br', "<cmd>lua require'dap'.clear_breakpoints()<cr>", { desc = "Clear breakpoints" })
+vim.keymap.set("n", '<leader>ba', '<cmd>Telescope dap list_breakpoints<cr>', { desc = "List breakpoints" })
+
+vim.keymap.set("n", "<leader>dj", "<cmd>lua require'dap'.step_over()<cr>", { desc = "Step over" })
+vim.keymap.set("n", "<leader>dk", "<cmd>lua require'dap'.step_into()<cr>", { desc = "Step into" })
+vim.keymap.set("n", "<leader>do", "<cmd>lua require'dap'.step_out()<cr>", { desc = "Step out" })
+vim.keymap.set("n", '<leader>dd', "<cmd>lua require'dap'.disconnect()<cr>", { desc = "Disconnect" })
+vim.keymap.set("n", '<leader>dt', "<cmd>lua require'dap'.terminate()<cr>", { desc = "Terminate" })
+vim.keymap.set("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", { desc = "Open REPL" })
+vim.keymap.set("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", { desc = "Run last" })
+vim.keymap.set("n", '<leader>di', function() require "dap.ui.widgets".hover() end, { desc = "Variables" })
+vim.keymap.set("n", '<leader>d?', function()
+    local widgets = require "dap.ui.widgets"; widgets.centered_float(widgets.scopes)
+end, { desc = "Scopes" })
+vim.keymap.set("n", '<leader>df', '<cmd>Telescope dap frames<cr>', { desc = "List frames" })
+vim.keymap.set("n", '<leader>dh', '<cmd>Telescope dap commands<cr>', { desc = "List commands" })
