331222d8c2cc44f66fe11aa3e0ee612353417611
Nick Marks <nickmarks00@gmail.com>
2024-03-12 18:09:36 +1100

    Add treesitter support for auto pair and matchup

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -81,4 +81,13 @@ require 'nvim-treesitter.configs'.setup {
       },
     },
   },
+
+  autotag = {
+    enable = true,
+  },
+  matchup = {
+    enable = true, -- mandatory, false will disable the whole extension
+    --disable = { "c", "ruby" },   -- optional, list of language that will be disabled
+    -- [options]
+  },
 }
