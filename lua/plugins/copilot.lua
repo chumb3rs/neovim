0d4b219ba0afe95bcbce6767332825b6908de00a
Nick Marks <nickmarks00@gmail.com>
2024-03-31 06:28:47 +1100

    Disable Copilot by default and add toggling keymaps

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,9 +1,14 @@
 return {
     "github/copilot.vim",
     version = "1.18.0",
+    cmd = "Copilot enable",
+    keys = {
+        { "<leader>coe", ":Copilot enable<CR>", mode = "n", desc = "Enable GitHub Copilot" }
+    },
     init = function()
         vim.g.copilot_no_tab_map = true
         vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+        vim.keymap.set("n", "<leader>cod", ":Copilot disable<CR>", { desc = "Disable GitHub Copilot" })
         vim.g.copilot_filetypes = {
             -- ["*"] = false,
             ["javascript"] = true,
