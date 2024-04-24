6af9f47b2fe395772e8b0ed9956df864aa39dcb4
Nick Marks <nickmarks00@gmail.com>
2024-04-24 10:14:58 +1000

    Remove non-plugin conflicting remaps

--- lua/chumbers/remap.lua
+++ lua/chumbers/remap.lua
@@ -67,5 +67,5 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>ed', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
