d75e3e17cb42072f59275c45e9f1ef260db21ef9
Nick Marks <nickmarks00@gmail.com>
2024-05-06 17:48:22 +1000

    Customise nvim-tree with relative line-numbering et al.

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -9,33 +9,80 @@ return {
     init = function()
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
-    end,
-    config = function()
-        require("nvim-tree").setup {
-            sort_by = "case_sensitive",
-            git = {
-                enable = true,
-            },
-            view = {
-                width = 30,
-            },
-            renderer = {
-                group_empty = true,
-                highlight_git = true,
-                icons = {
-                    show = {
-                        git = true,
-                    }
-                }
-            },
-            filters = {
-                dotfiles = true,
-            },
-        }
-
         vim.g.nvim_tree_follow = 1
         vim.cmd([[
             :hi! link NvimTreeExecFile NONE
         ]])
-    end
+
+        function my_on_attach(bufnr)
+            local api = require("nvim-tree.api")
+            vim.keymap.set("n", "l", api.node.open.edit, { desc = "Open folder/file" })
+            vim.keymap.set("n", "h", api.node.navigate.parent_close, { desc = "Close parent folder" })
+
+            api.config.mappings.default_on_attach(bufnr)
+        end
+    end,
+    opts = {
+        on_attach = my_on_attach,
+        hijack_cursor = true,
+        sort_by = "case_sensitive",
+        git = {
+            enable = false,
+            ignore = false,
+        },
+        diagnostics = {
+            enable = true,
+            show_on_dirs = true,
+            show_on_open_dirs = true,
+            icons =
+            {
+                hint = '',
+                info = "",
+                warning = "",
+                error = ""
+            }
+        },
+        view = {
+            width = 35,
+            relativenumber = true,
+            signcolumn = "yes",
+            cursorline = false,
+        },
+        renderer = {
+            group_empty = true,
+            root_folder_modifier = ":t",
+            highlight_git = "all",
+            highlight_diagnostics = "icon",
+            highlight_opened_files = "all",
+            highlight_modified = "name",
+            indent_markers = {
+                enable = true
+            },
+            icons = {
+                modified_placement = "before",
+                git_placement = "after",
+                show = {
+                    folder_arrow = false,
+                    modified = true,
+                },
+                glyphs = {
+                    default = "",
+                },
+            },
+        },
+        filters = {
+            custom = { "^\\.git", "^\\node_modules" }
+        },
+        modified = {
+            enable = true,
+        },
+        actions = {
+            change_dir = {
+                restrict_above_cwd = true,
+            },
+            open_file = {
+                resize_window = true
+            }
+        },
+    }
 }
