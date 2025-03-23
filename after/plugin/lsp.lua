6ed0a5a235cd610a0c69de76e86d3e5ae6d79fa7
Nick Marks <nickmarx@amazon.com>
2025-03-23 17:27:23 +1100

    Configure Bemol and Barium for Java development
    1. Barium adds syntax highlighting and linting to Brazil Config files
    2. Bemol provides typing/linting support for the Java LSP to work in
       Brazil workplaces.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -61,3 +61,24 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         vim.lsp.buf.format { async = false }
     end
 })
+
+-- For Bemol integration
+function bemol()
+    local bemol_dir = vim.fs.find({ '.bemol' }, { upward = true, type = 'directory' })[1]
+    local ws_folders_lsp = {}
+    if bemol_dir then
+        local file = io.open(bemol_dir .. '/ws_root_folders', 'r')
+        if file then
+            for line in file:lines() do
+                table.insert(ws_folders_lsp, line)
+            end
+            file:close()
+        end
+    end
+
+    for _, line in ipairs(ws_folders_lsp) do
+        vim.lsp.buf.add_workspace_folder(line)
+    end
+end
+
+bemol()
