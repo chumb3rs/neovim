72f8fc79b9efbc729fe622eba2e180a2e6609d48
nickmarks00 <nickmarks00@gmail.com>
2025-03-27 11:13:44 +1100

    Switch harpoon to harpoon2 branch
    I had seemingly missed that the original Harpoon version had been
    deprecated and switched to a new branch of development - `harpoon2`.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,18 +1,5 @@
 return {
-    "theprimeagen/harpoon",
-    init = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
-
-        vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon mark file" })
-        --vim.keymap.set("n", "<leader>r", mark.rm_file, { desc="Harpoon remove file"})
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoon toggle menu" })
-
-        vim.keymap.set("n", "<leader>h1", function() ui.nav_file(1) end, { desc = "Harpoon navigate file 1" })
-        vim.keymap.set("n", "<leader>h2", function() ui.nav_file(2) end, { desc = "Harpoon navigate file 2" })
-        vim.keymap.set("n", "<leader>h3", function() ui.nav_file(3) end, { desc = "Harpoon navigate file 3" })
-        vim.keymap.set("n", "<leader>h4", function() ui.nav_file(4) end, { desc = "Harpoon navigate file 4" })
-        vim.keymap.set("n", "<leader>h5", function() ui.nav_file(5) end, { desc = "Harpoon navigate file 5" })
-        vim.keymap.set("n", "<leader>h6", function() ui.nav_file(6) end, { desc = "Harpoon navigate file 6" })
-    end
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" }
 }
