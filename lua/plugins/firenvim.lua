0a3fd26995db0a46096cf313ced2fcf7c4462459
nickmarks00 <nickmarks00@gmail.com>
2025-04-17 14:37:54 +1000

    Migrate Firenvim settings to single file

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -1,12 +1,25 @@
 return {
-    'glacambre/firenvim',
+    "glacambre/firenvim",
     build = ":call firenvim#install(0)",
     config = function()
-
-        vim.api.nvim_create_autocmd({'BufEnter'}, {
+        vim.api.nvim_create_autocmd({ "BufEnter" }, {
             pattern = "github.com_*.txt",
-            command = "set filetype=markdown"
+            command = "set filetype=markdown",
         })
 
-    end
+        -- Keymaps specific to Firenvim
+        if vim.g.started_by_firenvim == true then
+            vim.keymap.set("i", "<D-v>", "<C-R><C-O>+", { desc = "Use Cmd+v to paste" })
+        end
+
+        vim.g.firenvim_config = {
+            globalSettings = { alt = "all" },
+            localSettings = {
+                [".*"] = {
+                    takeover = "never",
+                    cmdline = "neovim",
+                },
+            },
+        }
+    end,
 }
