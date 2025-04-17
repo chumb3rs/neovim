ba018f7ff09b13cc1d42250684ec69d20a24a4e1
Nick Marks <nickmarx@amazon.com>
2025-04-17 13:52:04 +1000

    Configure Telescope to use fzf_native

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,14 @@
 return {
     'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    extensions = {
+        fzf = {
+          fuzzy = true,                    -- false will only do exact matching
+          override_generic_sorter = true,  -- override the generic sorter
+          override_file_sorter = true,     -- override the file sorter
+          case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+        }
+    },
     config = function()
         -- Telescope
         local builtin = require('telescope.builtin')
@@ -43,5 +51,7 @@ return {
                 }
             }
         }
+
+        require("telescope").load_extension("fzf")
     end
 }
