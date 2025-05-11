a1943e461e5d02205017d6c76381d0ab3d718f65
Nick Marks <nickmarks00@gmail.com>
2025-05-11 20:09:44 +1000

    Clean up DAP config
    This commit submits a number of QOL fixes for my DAP setup. In
    particular it cleans up keymaps for working with DAP and the DAP UI.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,42 +1,41 @@
 return {
     "mfussenegger/nvim-dap",
-    cmd = "DapContinue",
-    keys = {
-        { "<leader>dr", "<cmd> DapContinue <CR>",         mode = "n", desc = "Start or continue debugging" },
-        { "<leader>db", "<cmd> DapToggleBreakpoint <CR>", mode = "n", desc = "Toggle breakpoint" },
-    },
     config = function()
         local dap = require("dap")
         local dapui = require("dapui")
 
         -- REMAPS
+        vim.keymap.set("n", "<F5>", dap.continue, { desc = "DAP Continue" })
+        vim.keymap.set("n", "<F10>", dap.step_over, { desc = "DAP Step Over" })
+        vim.keymap.set("n", "<F11>", dap.step_into, { desc = "DAP Step Into" })
+        vim.keymap.set("n", "<F12>", dap.step_out, { desc = "DAP Step Out" })
+
         vim.keymap.set("n", "<leader>dus", function()
             local widgets = require("dap.ui.widgets");
             local sidebar = widgets.sidebar(widgets.scopes);
             sidebar.open();
         end, { desc = "Open debugging sidebar" })
-        vim.keymap.set("n", "<leader>bc",
-            "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>",
-            { desc = "Set conditional breakpoint" })
-        vim.keymap.set("n", "<leader>bl",
-            "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>",
-            { desc = "Set log point" })
-        vim.keymap.set("n", '<leader>br', "<cmd>lua require'dap'.clear_breakpoints()<cr>", { desc = "Clear breakpoints" })
-        vim.keymap.set("n", '<leader>ba', '<cmd>Telescope dap list_breakpoints<cr>', { desc = "List breakpoints" })
+        vim.keymap.set("n", "<leader>dd", dap.disconnect, { desc = "DAP Disconnect" })
+        vim.keymap.set("n", "<leader>dtt", dap.terminate, { desc = "DAP Terminate" })
+        vim.keymap.set("n", "<Leader>dr", dap.repl.toggle, { desc = "Toggle REPL" })
+        vim.keymap.set("n", "<Leader>dl", dap.run_last, { desc = "Run Last DAP Session" })
 
-        vim.keymap.set("n", "<leader>dj", "<cmd>lua require'dap'.step_over()<cr>", { desc = "Step over" })
-        vim.keymap.set("n", "<leader>dk", "<cmd>lua require'dap'.step_into()<cr>", { desc = "Step into" })
-        vim.keymap.set("n", "<leader>do", "<cmd>lua require'dap'.step_out()<cr>", { desc = "Step out" })
-        vim.keymap.set("n", '<leader>dd', "<cmd>lua require'dap'.disconnect()<cr>", { desc = "Disconnect" })
-        vim.keymap.set("n", '<leader>dt', "<cmd>lua require'dap'.terminate()<cr>", { desc = "Terminate" })
-        --vim.keymap.set("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", { desc = "Open REPL" })
-        vim.keymap.set("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", { desc = "Run last" })
+        vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+        vim.keymap.set("n", "<Leader>dB", function()
+            dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+        end, { desc = "Set Conditional Breakpoint" })
+        vim.keymap.set("n", "<Leader>dl", function()
+            dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+        end, { desc = "Set Log Point" })
+        vim.keymap.set("n", "<leader>dcb", dap.clear_breakpoints, { desc = "(DAP) Clear breakpoints" })
+
         vim.keymap.set("n", '<leader>di', function() require "dap.ui.widgets".hover() end, { desc = "Variables" })
         vim.keymap.set("n", '<leader>d?', function()
             local widgets = require "dap.ui.widgets"; widgets.centered_float(widgets.scopes)
         end, { desc = "Scopes" })
-        vim.keymap.set("n", '<leader>df', '<cmd>Telescope dap frames<cr>', { desc = "List frames" })
-        vim.keymap.set("n", '<leader>dh', '<cmd>Telescope dap commands<cr>', { desc = "List commands" })
+        vim.keymap.set("n", "<leader>dlb", "<cmd>Telescope dap list_breakpoints<cr>", { desc = "(DAP) List breakpoints" })
+        vim.keymap.set("n", '<leader>dlf', '<cmd>Telescope dap frames<cr>', { desc = "List frames" })
+        vim.keymap.set("n", '<leader>dlc', '<cmd>Telescope dap commands<cr>', { desc = "List commands" })
         -- REMAPS
 
         dapui.setup()
