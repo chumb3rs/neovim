85599c7562f21a475199bd1b755b9629768fd843
nickmarks00 <nickmarks00@gmail.com>
2025-05-16 12:03:57 +1000

    Set signcolumn width wider to allow for gutter signs

--- lua/chumbers/set.lua
+++ lua/chumbers/set.lua
@@ -8,9 +8,13 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.numberwidth = 4
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = true
+
+vim.opt.fillchars = { eob = ' ' }
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -21,7 +25,7 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "yes:3" -- Reserve 3 columns for signs
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
