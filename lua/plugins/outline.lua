147d8be9c33b37229bd8e3249c5b97b755fadd5b
nickmarks00 <nickmarks00@gmail.com>
2025-07-04 21:19:53 +1000

    feat: Migrate symbols-outline.nvim to outline.nvim

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,19 @@
+return {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = { -- Example mapping to toggle outline
+        { "<leader>ol", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    },
+    opts = {
+        outline_window = {
+            position = 'right',
+            width = 25,
+            show_numbers = true,
+            show_relative_numbers = true,
+        },
+        outline_items = {
+            show_symbol_lineno = true,
+        }
+    },
+}
