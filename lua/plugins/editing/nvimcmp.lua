21c3bd30ecc97554f28d4898fb4d8a49aebfe391
Nick Marks <nickmarks00@gmail.com>
2025-07-12 08:35:20 +1000

    chore: Reorder nvimcmp sources to prioritise snippets
    I am going to experiment with this at work and see if I prefer it to
    having the LSP sources come first. I think realistically, I probably
    will revert this because my snippets should be done in such a way that
    they can automatically trigger and have triggers that don't overlap with
    common LSP commands.

--- lua/plugins/editing/nvimcmp.lua
+++ lua/plugins/editing/nvimcmp.lua
@@ -1,10 +1,10 @@
 return {
-    'hrsh7th/nvim-cmp',
+    "hrsh7th/nvim-cmp",
     config = function()
         local cmp = require("cmp")
         local luasnip = require("luasnip")
         local lspkind = require("lspkind")
-        require('luasnip.loaders.from_vscode').lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load()
         luasnip.config.setup()
 
         lspkind.init({})
@@ -17,20 +17,19 @@ return {
                 end,
             },
             enabled = function()
-                return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
-                    or require("cmp_dap").is_dap_buffer()
+                return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
             end,
-            mapping = cmp.mapping.preset.insert {
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete({ select = true }),
-                ["fj"] = cmp.mapping.confirm {
+            mapping = cmp.mapping.preset.insert({
+                ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete({ select = true }),
+                ["fj"] = cmp.mapping.confirm({
                     behavior = cmp.ConfirmBehavior.Replace,
                     select = true,
-                },
-                ['<Tab>'] = cmp.mapping(function(fallback)
+                }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
                     elseif luasnip.expand_or_locally_jumpable() then
@@ -38,8 +37,8 @@ return {
                     else
                         fallback()
                     end
-                end, { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
                     elseif luasnip.locally_jumpable(-1) then
@@ -47,26 +46,26 @@ return {
                     else
                         fallback()
                     end
-                end, { 'i', 's' }),
-            },
+                end, { "i", "s" }),
+            }),
             sources = {
-                { name = 'nvim_lsp' },
-                { name = 'ultisnips' },
+                { name = "ultisnips" },
+                { name = "nvim_lsp" },
                 { name = "buffer" },
-                { name = 'luasnip' },
+                { name = "luasnip" },
                 { name = "crates" },
-                { name = 'async_path' },
+                { name = "async_path" },
                 { name = "nvim_lsp_signature_help" },
-                { name = "lazydev", group_index = 0 }
+                { name = "lazydev",                group_index = 0 },
             },
 
             formatting = {
                 format = lspkind.cmp_format({
-                    mode = 'symbol_text',  -- show only symbol annotations
+                    mode = "symbol_text",  -- show only symbol annotations
                     maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-                    ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-                })
-            }
+                    ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                }),
+            },
         })
 
         require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
@@ -74,5 +73,5 @@ return {
                 { name = "dap" },
             },
         })
-    end
+    end,
 }
