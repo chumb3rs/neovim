013c19732d81b651f9752e0c98427c02a928f632
nickmarks00 <nickmarks00@gmail.com>
2025-07-11 10:20:25 +1000

    chore: Update outline.nvim remap to <leader>ol

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -3,7 +3,7 @@ return {
     lazy = true,
     cmd = { "Outline", "OutlineOpen" },
     keys = { -- Example mapping to toggle outline
-        { "<leader>ol", "<cmd>Outline<CR>", desc = "Toggle outline" },
+        { "<leader>os", "<cmd>Outline<CR>", desc = "Toggle outline" },
     },
     opts = {
         outline_window = {
