08b4e40135d7556ae87ab56c130529d46303a23e
nickmarks00 <nickmarks00@gmail.com>
2025-09-25 12:39:46 +1000

    chore: Fix GBrowse to work in visual mode with same remap

--- lua/plugins/git/fugitive.lua
+++ lua/plugins/git/fugitive.lua
@@ -4,6 +4,7 @@ return {
     keys = {
         { "<leader>gs",  "<cmd>vertical topleft Git | set winfixwidth | vertical resize 100<CR>", mode = "n", desc = "[G]it [S]tatus" },
         { "<leader>gbr", vim.cmd.GBrowse,         mode = "n", desc = "[G]it [B][R]owse" },
+        { "<leader>gbr", ":GBrowse<CR>", mode = "x", desc = "[G]it [B][R]owse" },
         { "<leader>gbl", "<cmd>Git blame<CR>",    mode = "n", desc = "[G]it [B][L]ame" },
         { "<leader>glg", "<cmd>vertical botright Git log | set winfixwidth | vertical resize 100<CR>", mode = "n", desc = "[G]it [L]o[G]" },
         { "<leader>gp",  "<cmd>Git pull<CR>",     mode = "n", desc = "[G]it [P]ull" },
