654724069d26ff70145b967a540cd7849d4627be
nickmarks00 <nickmarks00@gmail.com>
2025-11-04 14:15:40 +1100

    feat: Set filetype to markdown for all firenvim buffers

--- lua/plugins/utilities/firenvim.lua
+++ lua/plugins/utilities/firenvim.lua
@@ -2,14 +2,15 @@ return {
     "glacambre/firenvim",
     build = ":call firenvim#install(0)",
     config = function()
-        vim.api.nvim_create_autocmd({ "BufEnter" }, {
-            pattern = {"github.com_*.txt", "t.corp.amazon.com_*.txt"},
-            command = "set filetype=markdown",
-        })
-
         -- Keymaps specific to Firenvim
         if vim.g.started_by_firenvim == true then
             vim.keymap.set("i", "<D-v>", "<C-R><C-O>+", { desc = "Use Cmd+v to paste" })
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = 'text',
+                callback = function()
+                    vim.bo.filetype = 'markdown'
+                end
+            })
         end
 
         vim.g.firenvim_config = {
