6f68935ff090271e69d06f46d6426ea4e6092325
nickmarks00 <nickmarks00@gmail.com>
2026-05-12 10:24:45 +1000

    chore: Move away from deprecated vim.diagnostic.goto_next/prev
    I also changed the remap to something easier for homerow access

--- lua/chumbers/remap.lua
+++ lua/chumbers/remap.lua
@@ -64,10 +64,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>ed', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set("n", "dk", vim.diagnostic.jump({count=-1, float=true}), { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "dj", vim.diagnostic.jump({count=1, float=true}), { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>ed", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- Opening Tools
 vim.keymap.set("n", "<leader>lz", "<cmd>Lazy<CR>", { desc = "Open [L]a[z]y" })
