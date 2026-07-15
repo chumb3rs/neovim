7db69a45d0fa873018cb16c71d137de0e3bc888c
nickmarks00 <nickmarks00@gmail.com>
2026-07-15 11:34:30 +1000

    fix: Correct lazy-loading property of markview.nvim plugin

--- lua/plugins/ui/markview.lua
+++ lua/plugins/ui/markview.lua
@@ -1,7 +1,7 @@
 -- For `plugins/markview.lua` users.
 return {
     "OXY2DEV/markview.nvim",
-    lazy = true,
+    lazy = false,  -- do not lazy load plugin as it is already lazy-loaded
     opt = {
         experimental = {
             check_rtp_message = false
